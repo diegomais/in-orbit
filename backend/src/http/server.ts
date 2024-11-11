b44b0cc38d92b3d2d6a595c27bb79bb7b557c87b
Diego Mais <diegomais.pro@gmail.com>
2024-11-10 22:11:15 -0300

    feat(backend): configure fastify-type-provider-zod

--- backend/src/http/server.ts
+++ backend/src/http/server.ts
@@ -1,8 +1,17 @@
 import fastify from 'fastify'
+import {
+  type ZodTypeProvider,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
 import env from '@/config/env'
 
 const app = fastify()
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+app.withTypeProvider<ZodTypeProvider>()
 
 app.get('/', async (request, reply) => {
   reply.send({ message: 'Hello World!' })
